ac7cbdb7e0c31e51ef128e0ceac33ab502f2a0ac
Erol Cemiloglu <erolc@outlook.com.au>
2026-01-21 18:07:39 +1100

    testing exit text for projects

--- frontend/app/projects/page.tsx
+++ frontend/app/projects/page.tsx
@@ -143,6 +143,11 @@ export default function ProjectsPage() {
             );
           })}
         </div>
+        
+        <h3 className="text-4xl font-bold mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400">
+            Projects & Experience
+        </h3>
+
       </div>
     </main>
   );
